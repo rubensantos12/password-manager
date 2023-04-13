5f76b387c833a9fef707ca07fb6689cc8d5f0387
rubensantos12 <rubensantos889@gmail.com>
2023-04-13 16:30:25 +0000

    Added new method to retrieve all passwords at once and decrypted for the user and a test method that returns the password encrypted
    Also added comments to the controller class

--- src/main/java/com/rubensantos/password/manager/Controller/PasswordController.java
+++ src/main/java/com/rubensantos/password/manager/Controller/PasswordController.java
@@ -3,13 +3,14 @@ package com.rubensantos.password.manager.Controller;
 import com.rubensantos.password.manager.Encryption.PasswordEncryption;
 import com.rubensantos.password.manager.Entity.Password;
 import com.rubensantos.password.manager.Repository.PasswordRepo;
-import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,6 +21,12 @@ public class PasswordController {
 
     PasswordEncryption passwordEncryption = new PasswordEncryption();
 
+    /**
+     *
+     * @param id ID of the password saved
+     * @return The password with said ID decrypted
+     */
+
     @GetMapping("/getPassword/{id}")
     public Object getPassword(@PathVariable(name="id") Integer id) {
         Optional<Password> encryptedPassword = passwordRepo.findById(id);
@@ -29,12 +36,52 @@ public class PasswordController {
         return decryptedPassword;
     }
 
+    /**
+     *
+     * @return A List of all usernames and passwords saved on the database
+     */
+
+    @GetMapping("/getAllPasswords")
+    public List<Password> getAllPasswords() {
+        Iterable<Password> listOfEncryptedPasswords = passwordRepo.findAll();
+        List<Password> listOfDecryptedPasswords = new ArrayList<>();
+
+        for (Password passwordToDecrypt : listOfEncryptedPasswords) {
+            System.out.println(passwordToDecrypt.getPassword());
+            Password passwordToSave = new Password();
+            passwordToSave.setPassword(passwordEncryption.decryptPassword(passwordToDecrypt.getPassword()));
+            passwordToSave.setUsername(passwordToDecrypt.getUsername());
+            passwordToSave.setId(passwordToDecrypt.getId());
+            listOfDecryptedPasswords.add(passwordToSave);
+        }
+
+        return listOfDecryptedPasswords;
+    }
+
+    /**
+     *
+     * @param username Desired username used to save
+     * @param password Desired password used to save
+     */
+
     @PostMapping("/savePassword/{username}/{password}")
-    public void savePassword(@PathVariable(name="username") String username, @PathVariable(name="password") String password) {
+    public String savePassword(@PathVariable(name="username") String username, @PathVariable(name="password") String password) {
         Password passwordToSave = new Password();
         passwordToSave.setPassword(passwordEncryption.encryptPassword(password));
         passwordToSave.setUsername(username);
         passwordRepo.save(passwordToSave);
+        return "Password successfully saved!";
+    }
+
+    /**
+     *
+     * @param id The ID of the Password saved
+     * @return The Password completely encrypted
+     */
+
+    @GetMapping("/getEncryptedPassword/{id}")
+    public Object getEncryptedPassword(@PathVariable(name="id") Integer id) {
+        return passwordRepo.findById(id);
     }
 
 }
